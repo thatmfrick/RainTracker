ec0886c057441ad3abaf87b580bd806b969cd929
rick <thatmfrick@proton.me>
2026-07-24 01:08:01 +0200

    Adding virtual zoom + global variables to be edited by user (in the future)

--- lib/core.sh
+++ lib/core.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/env bash
 
+TILE_SIZE=512
+PICTURE_SIZE=512
+ZOOM=7
+VIRTUAL_ZOOM=4
+SCALE_FACTOR=$((2 ** VIRTUAL_ZOOM))
+CROP_SIZE=$((PICTURE_SIZE / SCALE_FACTOR))
+CROP_X=$(((PICTURE_SIZE - CROP_SIZE) / 2))
+CROP_Y=$(((PICTURE_SIZE - CROP_SIZE) / 2))
+
 fetch_data() {
-    local RAINVIEWER_API='https://api.rainviewer.com/public/weather-maps.json' RESPONSE TIME LAST_TIME=99 IMAGE_PATH RAINVIEWER_PIC POLYGON_PIC PICTURE_SIZE=512 ZOOM=7 LAT LON
+    local RAINVIEWER_API='https://api.rainviewer.com/public/weather-maps.json' RAINVIEWER_PIC POLYGON_PIC COMPOSITE_PIC CROPPED_RADAR RESPONSE TIME LAST_TIME=99 IMAGE_PATH LAT LON
 
     RAINVIEWER_PIC=$(mktemp --suffix=.png)
     POLYGON_PIC=$(mktemp --suffix=.png)
+    COMPOSITE_PIC=$(mktemp --suffix=.png)
+    CROPPED_RADAR=$(mktemp --suffix=.png)
 
-    IFS=, read -r LAT LON <florida.csv
+    IFS=, read -r LAT LON <test.csv
 
     draw_polygon "$POLYGON_PIC" "$LAT" "$LON" &
+    POLYGON_PID=$!
 
     while true; do
         RESPONSE=$(curl -sf "$RAINVIEWER_API")
@@ -16,7 +28,22 @@ fetch_data() {
         IMAGE_PATH="$(jq -r '.host + .radar.past[-1].path' <<<"$RESPONSE")"
         if ((10#$TIME != 10#$LAST_TIME)); then
             curl -sf "$IMAGE_PATH/$PICTURE_SIZE/$ZOOM/$LAT/$LON/0/1_1.png" >"$RAINVIEWER_PIC"
-            magick "$RAINVIEWER_PIC" "$POLYGON_PIC" -composite png:- | chafa --clear --polite on --probe off
+
+            magick "$RAINVIEWER_PIC" \
+                -crop "${CROP_SIZE}x${CROP_SIZE}+${CROP_X}+${CROP_Y}" \
+                +repage \
+                -filter point \
+                -resize "${PICTURE_SIZE}x${PICTURE_SIZE}" \
+                "$CROPPED_RADAR"
+
+            magick -size "${PICTURE_SIZE}x${PICTURE_SIZE}" xc:none \
+                "$CROPPED_RADAR" -composite \
+                "$POLYGON_PIC" -composite \
+                "$COMPOSITE_PIC"
+
+            tput clear
+            # tput cup $(((LINES - 30) / 2)) $(((COLUMNS - 50) / 2))
+            chafa --polite on --probe off "$COMPOSITE_PIC"
             LAST_TIME=$TIME
         fi
         sleep 60
@@ -24,47 +51,54 @@ fetch_data() {
 }
 
 latlon_to_pixel() {
-    local LAT="$1" LON="$2" TILE_SIZE=256 ZOOM=7
-    awk -v lat="$LAT" -v lon="$LON" -v tile_size="$TILE_SIZE" -v zoom="$ZOOM" '
+    local LAT="$1" LON="$2"
+    awk -v lat="$LAT" -v lon="$LON" -v tile_size="$TILE_SIZE" -v zoom="$((ZOOM + VIRTUAL_ZOOM))" '
         BEGIN {
+            if (lat > 85.051129)  lat = 85.051129
+            if (lat < -85.051129) lat = -85.051129
+            
             pi = atan2(0, -1)
             scale = 2 ^ zoom
+            lat_rad = lat * (pi / 180)
+            
+            ang = (pi / 4) + (lat_rad / 2)
+            tan_val = sin(ang) / cos(ang)
 
-            siny = sin(lat * pi / 180)
-            if (siny > 0.9999) siny = 0.9999
-            if (siny < -0.9999) siny = -0.9999
-
-            x = tile_size * (0.5 + lon / 360) * scale
-            y = tile_size * (0.5 - log((1 + siny) / (1 - siny)) / (4 * pi)) * scale
-
+            x = tile_size * scale * (( lon + 180) / 360)
+            y = tile_size * scale * (0.5 - log(tan_val) / (2 * pi))
+            
             printf "%.0f %.0f\n", x, y
         }
     '
 }
 
 draw_polygon() {
-    local POLYGON_PIC="$1" CENTER_LAT="$2" CENTER_LON="$3" TILE_SIZE=256 IMAGE_SIZE=512 POLYGON=""
+    local POLYGON_PIC="$1" CENTER_LAT="$2" CENTER_LON="$3" POLYGON=""
     read -r CENTER_X CENTER_Y <<<"$(latlon_to_pixel "$CENTER_LAT" "$CENTER_LON")"
 
     while IFS=, read -r LAT LON; do
         read -r WORLD_X WORLD_Y <<<"$(latlon_to_pixel "$LAT" "$LON")"
 
-        PX=$(awk -v x="$WORLD_X" -v cx="$CENTER_X" -v tile_size="$TILE_SIZE" -v img_size="$IMAGE_SIZE" '
+        PX=$(awk -v x="$WORLD_X" -v cx="$CENTER_X" -v tile_size="$TILE_SIZE" -v img_size="$PICTURE_SIZE" '
                 BEGIN {
                     printf "%.0f", img_size/2 + (x - cx) * (tile_size/img_size) 
                 }
             ')
 
-        PY=$(awk -v y="$WORLD_Y" -v cy="$CENTER_Y" -v tile_size="$TILE_SIZE" -v img_size="$IMAGE_SIZE" '
+        PY=$(awk -v y="$WORLD_Y" -v cy="$CENTER_Y" -v tile_size="$TILE_SIZE" -v img_size="$PICTURE_SIZE" '
                 BEGIN { 
                     printf "%.0f", img_size/2 + (y - cy) * (tile_size/img_size) 
                 }
             ')
 
         POLYGON+="$PX,$PY "
-    done <florida.csv
+    done <test.csv
+    #usable 2 points only -> rectangle and circle
+    magick -size ${PICTURE_SIZE}x${PICTURE_SIZE} xc:none -fill none -stroke lime -strokewidth 1 -draw "circle $POLYGON" -stroke red -strokewidth 1 -draw "rectangle 0,0 $((PICTURE_SIZE - 1)),$((PICTURE_SIZE - 1))" "$POLYGON_PIC"
+}
 
-    magick -size 512x512 xc:none -fill "rgba(0,255,0,0.3)" -stroke lime -strokewidth 2 -draw "polygon $POLYGON" "$POLYGON_PIC"
+calculate_color() {
+    magick image.png -format "%[hex:u.p{19,14}]\n" info:
 }
 
 # Every tile is 256px, at zoom level 7 we will have 2^7 tiles = 128x128 grids so 256*2^7=32768px that will cover the whole world.
