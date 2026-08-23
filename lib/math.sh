614641d0472984970bc56e16dc0d2a71ae2ec796
rick <thatmfrick@proton.me>
2026-08-24 00:26:42 +0200

    Fixing and adding some math functions

--- lib/math.sh
+++ lib/math.sh
@@ -1,82 +1,105 @@
-latlon_to_pixel_calculi() {
-    local lat="$1" lon="$2" tile_size="$3" zoom="$4"
-    awk -v lat="$lat" -v lon="$lon" \
-        -v tile_size="$tile_size" -v zoom="$zoom" '
-        BEGIN {
-            if (lat > 85.051129)  lat = 85.051129
-            if (lat < -85.051129) lat = -85.051129
-            
-            pi = atan2(0, -1)
-            scale = 2 ^ zoom
-            lat_rad = lat * (pi / 180)
-            
-            ang = (pi / 4) + (lat_rad / 2)
-            tan_val = sin(ang) / cos(ang)
-
-            x = tile_size * scale * (( lon + 180) / 360)
-            y = tile_size * scale * (0.5 - log(tan_val) / (2 * pi))
-            
-            printf "%.0f %.0f\n", x, y
-        }
-    '
-}
-
 zoom_calculi() {
-    local full_zoom="$1"
+    local virtual_zoom="$1"
 
-    awk -v full_zoom="$full_zoom" -v picture_size="$PICTURE_SIZE" '
+    awk -v virtual_zoom="$virtual_zoom" -v picture_size="$PICTURE_SIZE" '
         BEGIN {
-            if (full_zoom > 7) {
-                api_zoom = 7 
-                virtual_zoom = full_zoom - 7
-            }
-
-            if (full_zoom <= 7) {
-                api_zoom = full_zoom 
-                virtual_zoom = 0
-            }
-
             scale_factor = 2 ^ virtual_zoom
             crop_size = picture_size / scale_factor
             crop_x = (picture_size - crop_size) / 2
             crop_y = (picture_size - crop_size) / 2
 
-            printf "%.0f %.0f %.0f %.0f %.0f\n", api_zoom, virtual_zoom, crop_size, crop_x, crop_y
+            print crop_size, crop_x, crop_y
+        }
+    '
+}
+
+# Web Mercator
+latlon_to_tile() {
+    local lat="$1" lon="$2"
+    local zoom="$3"
+
+    awk -v lat="$lat" -v lon="$lon" -v zoom="$zoom" '
+        function sec(x) { return 1/cos(x) }
+        function tan(x) { return sin(x) / cos(x) }
+        BEGIN {
+            PI = atan2(0, -1)
+            lat_rad = lat * (PI / 180)
+            n = 2^zoom  
+
+            x_float = n * (lon + 180) / 360
+            x_tile = int(x_float)
+            
+            y_float = n * (1 - (log(tan(lat_rad) + sec(lat_rad)) / PI)) / 2
+            y_tile = int(y_float)
+
+            left_offset = int((x_float-x_tile) * 256)
+            top_offset = int((y_float - y_tile) * 256)
+
+            print x_tile, y_tile, left_offset, top_offset
+        }
+    '
+}
+
+# source: https://medium.com/@suverov.dmitriy/how-to-convert-latitude-and-longitude-coordinates-into-pixel-offsets-8461093cb9f5
+
+latlon_to_pixel() {
+    local lat="$1" lon="$2" zoom="$3"
+    awk -v lat="$lat" -v lon="$lon" \
+        -v tile_size="$TILE_SIZE" -v zoom="$zoom" '
+        function tan(x) { return sin(x) / cos(x) }
+        BEGIN {
+            mapWidth = tile_size * 2^zoom
+            mapHeight = mapWidth
+            FE = 180
+            PI = atan2(0, -1)
+            radius = mapWidth / (2 * PI)
+
+            lonRad = ((lon + FE) * PI) /180
+            x = int(lonRad * radius)
+
+            latRad = (lat * PI) / 180
+            verticalOffsetFromEquator = radius * log(tan(PI/4 + latRad /2))
+
+            y = int(mapHeight / 2 - verticalOffsetFromEquator)
+
+            print x, y
         }
     '
 }
 
 pixel_coords() {
-    local center_lat="$1" center_lon="$2"
-    local tile_size="$3" PICTURE_SIZE="$4"
-    local zoom="$5" csv_file="$6" polygon=""
-    local center_x center_y world_x world_y
+    local c_px="$1" c_py="$2"
+    local zoom="$3" csv_file="$4" polygon=""
+    local world_x world_y
 
-    read -r center_x center_y <<<"$(latlon_to_pixel_calculi "$center_lat" "$center_lon" "$tile_size" "$zoom")"
+    #having to run another time inorder to read last line
+    while IFS=, read -r lat lon || [[ -n "$lat" ]]; do
+        read -r world_x world_y <<<"$(latlon_to_pixel "$lat" "$lon" "$zoom")"
 
-    while IFS=, read -r lat lon; do
-        read -r world_x world_y <<<"$(latlon_to_pixel_calculi "$lat" "$lon" "$tile_size" "$zoom")"
-
-        px=$(awk -v x="$world_x" -v cx="$center_x" -v img_size="$PICTURE_SIZE" '
+        px=$(awk -v x="$world_x" -v c_px="$c_px" -v img_size="$PICTURE_SIZE" '
                 BEGIN {
-                    printf "%.0f", img_size/2 + (x - cx)
+                    px = int(img_size/2 + (x - c_px))
+                    print px
                 }
             ')
 
-        py=$(awk -v y="$world_y" -v cy="$center_y" -v img_size="$PICTURE_SIZE" '
+        py=$(awk -v y="$world_y" -v c_py="$c_py" -v img_size="$PICTURE_SIZE" '
                 BEGIN { 
-                    printf "%.0f", img_size/2 + (y - cy)
+                    py = int(img_size/2 + (y - c_py))
+                    print py
                 }
             ')
 
-        polygon+="$px,$py"$'\n'
+        polygon+="$px,$py "
     done <"$csv_file"
 
     echo "$polygon"
 }
 
+# Will return the centered lat,lon point of the relative shape
 centroid_radar() {
     local csv_file="$1"
+
     awk -F, '{
         sum_lat += $1
         sum_lon += $2
@@ -84,16 +107,4 @@ centroid_radar() {
     } END {
         print sum_lat / count, sum_lon / count
     }' "$csv_file"
-}
-
-centroid_poly() {
-    local poly="$1"
-
-    awk -F, '{
-        sum_lat += $1
-        sum_lon += $2
-        count++
-    } END {
-        printf "%.0f %.0f", sum_lat / count, sum_lon / count
-    }' <<<"$poly"
 }
